a01662458d22fc5345fbc01edc5fda0aeef0795c
Matthew Jaskula <mjaskula@squarespace.com>
2014-07-30 10:39:54 -0400

    manually merged in my sublime settings, added keymap

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -612,7 +612,7 @@ defaults write com.apple.messageshelper.MessageController SOInputLineSettings -d
 ###############################################################################
 
 # Install Sublime Text settings
-cp -r ${ZSH}/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+cp -r ${ZSH}/sublime/User/* ~/Library/Application\ Support/Sublime\ Text*/Packages/User/ 2> /dev/null
 
 ###############################################################################
 # Kill affected applications                                                  #
