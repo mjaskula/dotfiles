ffa0c45643a734ff937766125a7d3a963ffd1998
Matthew Jaskula <mjaskula@squarespace.com>
2015-06-25 10:17:12 -0400

    [java] update opts for java 8

--- java/env.zsh
+++ java/env.zsh
@@ -2,5 +2,5 @@
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
-export MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -Xmn512M -Xmx2048M -XX:MaxPermSize=512M"
-export GRADLE_OPTS="-Xmx1024M -XX:MaxPermSize=1024M -Dorg.gradle.daemon=true"
+export MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -Xmn512M -Xmx2048M"
+export GRADLE_OPTS="-Xmx1024M -Dorg.gradle.daemon=true"
