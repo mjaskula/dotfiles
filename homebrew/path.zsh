ad08fb647a6a71817ef53837ba0aa0f1cbdd386d
Matthew Jaskula <mjaskula@squarespace.com>
2016-05-09 16:09:27 -0400

    point to postgress latest

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -4,6 +4,6 @@ export MANPATH="/usr/local/man:$MANPATH"
 export PATH=`echo $(brew --prefix coreutils)/libexec/gnubin`:$PATH
 export PATH=/usr/local/share/python:$PATH
 export PATH=$PATH:/usr/local/opt/ruby/bin
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 export PATH=$PATH:/opt/local/lib/mysql55/bin
 
