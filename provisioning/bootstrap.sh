48b41a3dd36b9e3b86fc997eb53985c088ab3b3a
Matthew Jaskula <mjaskula@squarespace.com>
2016-05-10 15:16:51 -0400

    Revert "trying a simpler bootstrap"
    This reverts commit 6bdc08d015bee363e248f8a77b8b8811786a5b23.

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -3,22 +3,19 @@
 xcode-select --install
 set -e
 
-sudo easy_install pip
-sudo pip install ansible
+echo "  fixing ownership of /usr/local"
+sudo chown -R $(whoami):admin /usr/local
 
-# echo "  fixing ownership of /usr/local"
-# sudo chown -R $(whoami):admin /usr/local
+if test ! $(which brew); then
+  echo "  Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-# if test ! $(which brew); then
-#   echo "  Installing Homebrew"
-#   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# fi
+#TODO: update the PATH? export PATH=/usr/local/bin:$PATH
 
-# #TODO: update the PATH? export PATH=/usr/local/bin:$PATH
+if test ! $(which ansible); then
+  echo "  Installing ansible"
+  brew install ansible
+fi
 
-# if test ! $(which ansible); then
-#   echo "  Installing ansible"
-#   brew install ansible
-# fi
-
-# ansible-pull --full -d .dotfiles -U https://github.com/mjaskula/dotfiles.git provisioning/provision.yml
+ansible-pull --full -d .dotfiles -U https://github.com/mjaskula/dotfiles.git provisioning/provision.yml
