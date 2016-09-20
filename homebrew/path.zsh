8ef5b5f5eceddfbcba49844b73d1c152b937494b
Matthew Jaskula <mjaskula@squarespace.com>
2016-09-20 11:26:06 -0400

    fix postgres path order

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,9 +1,9 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
 
+export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
 export PATH=`echo $(brew --prefix coreutils)/libexec/gnubin`:$PATH
 export PATH=/usr/local/share/python:$PATH
 export PATH=$PATH:/usr/local/opt/ruby/bin
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 export PATH=$PATH:/opt/local/lib/mysql55/bin
 
