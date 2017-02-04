8116dfa99bbb1b78947426586981be49cf7042bb
Matthew Jaskula <mjaskula@gmail.com>
2017-02-03 23:25:01 -0500

    backup: photos to amazon prime photos

--- system/backup.zsh
+++ system/backup.zsh
@@ -6,3 +6,5 @@ alias backupPicturesWD="time backup ~/Pictures/     /Volumes/Public/Backup/Heron
 alias backupMusicWD="time backup    ~/Music/iTunes/ /Volumes/Public/Backup/Heron/Music/iTunes"
 alias backupMoviesWD="time backup   ~/Movies/       /Volumes/Public/Backup/Heron/Movies/"
 alias backupDocsWD="time backup   ~/Documents/      /Volumes/Public/Backup/Heron/Documents/"
+
+alias backupPicturesAmazon="rclone sync -v --bwlimit 1M --include '*.{JPG,jpg,jpeg,PNG,png,tiff}' Pictures/Photos\ Library.photoslibrary/Masters amazon_drive:photobackup"
