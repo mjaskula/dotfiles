a0d4efebc71f8f834e96298e5cfabf7948f7db4d
Matthew Jaskula <mjaskula@gmail.com>
2018-02-23 01:17:10 -0500

    backup using rclone

--- system/backup.zsh
+++ system/backup.zsh
@@ -1,10 +1,6 @@
-alias backupMovies="time backup   ~/Movies/       matthew@Orion.local:Movies"
-alias backupMusic="time backup    ~/Music/iTunes/ matthew@Orion.local:Music/iTunes"
-alias backupPictures="time backup ~/Pictures/     matthew@Orion.local:Pictures"
+alias backupMusic="backup    Music/iTunes"
+alias backupPictures="backup Pictures"
+alias backupMovies="backup   Movies"
+alias backupDocs="backup     Documents"
 
-alias backupPicturesWD="time backup ~/Pictures/     /Volumes/Public/Backup/Heron/Pictures/"
-alias backupMusicWD="time backup    ~/Music/iTunes/ /Volumes/Public/Backup/Heron/Music/iTunes"
-alias backupMoviesWD="time backup   ~/Movies/       /Volumes/Public/Backup/Heron/Movies/"
-alias backupDocsWD="time backup   ~/Documents/      /Volumes/Public/Backup/Heron/Documents/"
-
-alias backupPicturesAmazon="rclone sync -v --bwlimit 1M --include '*.{JPG,jpg,jpeg,PNG,png,tiff}' Pictures/Photos\ Library.photoslibrary/Masters amazon_drive:photobackup"
+alias backupPicturesAmazon="rclone sync -v --bwlimit 1M --include '*.{JPG,jpg,jpeg,PNG,png,tiff}' ~/Pictures/Photos\ Library.photoslibrary/Masters amazon_drive:photobackup"
