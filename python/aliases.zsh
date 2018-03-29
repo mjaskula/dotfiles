4155fde694869b81d17017d36c4383a33f19c2ad
Matthew Jaskula <mjaskula@squarespace.com>
2018-03-29 11:16:30 -0400

    add flake8-watch, change ptpython color scheme

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,2 +1,5 @@
 alias flake8_changed_files='flake8 $(git diff --name-only $(git branchpoint))'
 alias flake8_mychanges='git diff master | flake8 --diff'
+alias flake8-watch='fswatch -o -l 5 -i "*.py" -e ".mypy_cache" -e ".git" -e ".idea" -e ".cache" . | xargs -n1 -I{} flake8 --statistics --count'
+
+
