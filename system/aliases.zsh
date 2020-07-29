8489d0d658e650f1848d086d7e3b2716401ad387
Matthew Jaskula <mjaskula@gmail.com>
2020-07-28 22:36:04 -0400

    remove unused aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,7 +9,6 @@ then
   alias la='gls -A --color'
 fi
 
-#alias ls='ls -G'
 alias pwdc='echo -n `pwd` | pbcopy'
 alias gpull='git stash save -u && git pull && git stash pop'
 alias mkdir="mkdir -p"
@@ -17,9 +16,6 @@ alias ag='ag --color'
 
 # misc
 
-alias purge_queues='rabbitmqadmin -f tsv -q list queues name | while read queue; do rabbitmqadmin purge queue name=${queue}; done'
 alias serve='python -m SimpleHTTPServer'
 
-# today scripts
 alias ips='dig +short myip.opendns.com @resolver1.opendns.com && ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d" " -f2'
-alias ts_cal='cal | grep --before-context 6 --after-context 6 --color -e " $(date +%e)" -e "^$(date +%e)"'
