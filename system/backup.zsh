4110b5717a5221545f8aa970c880d033a53af2c4
Matthew Jaskula <mjaskula@gmail.com>
2022-01-09 11:30:19 -0500

    switch backup from rclone to rsync

--- system/backup.zsh
+++ system/backup.zsh
@@ -1,6 +1,8 @@
-alias backupMusic="backup    Music/iTunes"
-alias backupPictures="backup Pictures"
-alias backupMovies="backup   Movies"
-alias backupDocs="backup     Documents"
+BACKUP_ROOT="Storage/Backup/Heron"
+
+alias backupPictures="backup Pictures     ${BACKUP_ROOT}"
+alias backupMovies="backup   Movies       ${BACKUP_ROOT}"
+alias backupDocs="backup     Documents    ${BACKUP_ROOT}"
+alias backupMusic="rsync -rlptDivxzP ~/Music/iTunes rsync://matthew@192.168.42.2:/Public/Shared\ Music/"
 
 alias backupPicturesAmazon="rclone sync -v --bwlimit 1M --include '*.{JPG,jpg,jpeg,PNG,png,tiff}' ~/Pictures/Photos\ Library.photoslibrary/Masters amazon_drive:photobackup"
