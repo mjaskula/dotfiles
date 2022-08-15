5a06ba95ea40a2092a2a2d4e93882f813457418a
Matthew Jaskula <mjaskula@gmail.com>
2022-08-15 15:54:37 -0400

    cleanup some brew path and setup

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,11 +1,11 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:$MANPATH"
 
-export PATH=/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH
 export PATH=`echo $(brew --prefix coreutils)/libexec/gnubin`:$PATH
 export PATH=/usr/local/share/python:$PATH
 export PATH=$PATH:/usr/local/opt/ruby/bin
-export PATH=$PATH:/opt/local/lib/mysql55/bin
 
 
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$(brew --prefix openssl)/lib
