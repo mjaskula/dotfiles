75c2107ad9a2effeed67a13c9cf1e79408e2a6d6
Matthew Jaskula <mjaskula@gmail.com>
2024-05-30 14:27:16 -0400

    update backup scripts
    now backing up to local external HDD instead of NAS
    encode the backup structure in the scripts

--- system/backup.zsh
+++ system/backup.zsh
@@ -1,8 +1,5 @@
-BACKUP_ROOT="Storage/Backup/Heron"
 
-alias backupPictures="backup Pictures     ${BACKUP_ROOT}"
-alias backupMovies="backup   Movies       ${BACKUP_ROOT}"
-alias backupDocs="backup     Documents    ${BACKUP_ROOT}"
-alias backupMusic="rsync -rlptDivxzP ~/Music/iTunes rsync://matthew@192.168.42.2:/Public/Shared\ Music/"
+alias backupT9="backup /Volumes/T9/storage T9 coldstorage"
 
 alias backupPicturesAmazon="rclone sync -v --bwlimit 1M --include '*.{JPG,jpg,jpeg,PNG,png,tiff}' ~/Pictures/Photos\ Library.photoslibrary/Masters amazon_drive:photobackup"
+
