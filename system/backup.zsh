e9032264b93e3d03cae795023709d692ad7db349
Matthew Jaskula <mjaskula@gmail.com>
2024-12-18 15:02:34 -0500

    fix iphone backup

--- system/backup.zsh
+++ system/backup.zsh
@@ -1,7 +1,7 @@
 
 alias backupT9="backup /Volumes/T9/storage T9 coldstorage"
 
-alias backupiPhones='backup ~/Library/Application\ Support/MobileSync/Backup "`hostname -s`/`whoami`/Library/Application Support/MobileSync/" T9'
+alias backupiPhones='backup ~/Library/Application\ Support/MobileSync/Backup "`hostname -s`/`whoami`/Library/Application Support/MobileSync/"'
 
 alias backupPicturesAmazon="rclone sync -v --bwlimit 1M --include '*.{JPG,jpg,jpeg,PNG,png,tiff}' ~/Pictures/Photos\ Library.photoslibrary/Masters amazon_drive:photobackup"
 
